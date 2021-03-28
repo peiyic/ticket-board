2828a17639d328e6b652ec3ed61e797e57ff664a
Peiyi Chi <peiyichi1@gmail.com>
2021-03-28 22:23:57 +1300

    test api
    Signed-off-by: Peiyi Chi <peiyichi1@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_BOARD = gql`
+query board($organisationId: ID!, $boardId: ID!) {
+  board(organisationId: $organisationId, boardId: $boardId) {
+    id
+    name
+    
+    createdAt
+    updatedAt
+    tickets {
+      name
+      description
+      status
+    }  
+  }
+}`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { loading, error, data, refetch, networkStatus } = useQuery(GET_BOARD, {
+    variables: {
+      "organisationId": "806fb7b1-64fb-4ec1-853b-f4ac7554cc64",
+      "boardId": "ac07246f-d903-408f-8506-80a3f67327d0"
+    },
+    notifyOnNetworkStatusChange: true
+  });
+  console.log("loading:" + loading);
+  console.log("error:" + error);
+  console.log("data:" + data);
+  console.log("refresh:" + refetch);
+  console.log("networkStatus:" + networkStatus)
+return (<div>{data}</div>);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,60 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {SubscriptionClient} from 'subscriptions-transport-ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { ApolloProvider, 
+         ApolloClient, 
+         InMemoryCache, 
+         HttpLink, 
+         split } from '@apollo/client';
+import { WebSocketLink } from "@apollo/client/link/ws";
+var base64 = require('base-64');
+
+// Create an http link:
+const httpLink = new HttpLink({
+  uri: 'https://w6tcrg3sb4.execute-api.us-east-1.amazonaws.com/example-example-graphql-api',
+  headers: {
+    'Authorization': base64.encode('123')
+  }
+});
+
+// Create a WebSocket link:
+const wsLink = new WebSocketLink(new SubscriptionClient(`wss://o8jmtfhs88.execute-api.ap-southeast-2.amazonaws.com/peiyi`, {
+  reconnect: true,
+  connectionParams: async () => {
+    return {
+      Authorization: base64.encode('123')
+    }
+  }
+}, undefined, []));
+
+const link = split(
+  // split based on operation type
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const gqlClient = new ApolloClient(
+  {
+      link: link,
+      cache: new InMemoryCache()
+  }
+);
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={gqlClient}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
